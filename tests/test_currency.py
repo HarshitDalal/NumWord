e17d726b429a37f94b918aa3ef1e536a0d3e29ad
HarshitDalal <harshitdalal96@gmail.com>
2025-02-07 01:24:16 +0530

    Updated test cases for test currency conversion

--- tests/test_currency.py
+++ tests/test_currency.py
@@ -40,25 +40,6 @@ class TestCurrency(unittest.TestCase):
         self.logger.info(f"Test Exchange rates load successfully: Total rates found -> {len(rates)}")
         self.assertGreater(len(rates), 0)
 
-    def test_exchange_rates_file_persistence(self):
-        old_currency_data = self.currency._Currency__load_exchange_rates()
-
-        currency_data = {"date": "2025-02-07", "rates": {"USD": 1.0, "EUR": 0.85}, "Base": "USD"}
-        self.currency._Currency__save_exchange_rates(currency_data)
-
-        file_path = self.currency._Currency__FILE_NAME
-        self.logger.info("Test exchange rate file available or not")
-        self.assertTrue(os.path.exists(file_path))
-
-        loaded_data = self.currency._Currency__load_exchange_rates()
-
-        self.logger.info(f"Test USD Exchange Rates -> {loaded_data['rates']['USD']}")
-        self.assertEqual(loaded_data["rates"]["USD"], 1.0)
-        self.logger.info(f"Test EUR Exchange Rates -> {loaded_data['rates']['EUR']}")
-        self.assertEqual(loaded_data["rates"]["EUR"], 0.85)
-
-        self.currency._Currency__save_exchange_rates(old_currency_data)
-
     def test_fetch_exchange_rates(self):
 
         data = self.currency._Currency__fetch_exchange_rates()
